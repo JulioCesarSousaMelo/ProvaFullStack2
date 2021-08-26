7fd2ce0bc9935b1f8175733653753e9b60411019
JulioCesarSousaMelo <jcesarsm17@gmail.com>
2021-08-26 18:40:37 -0300

    ==public +alteracao no form do index.php +novo arquivo cadastro.php

---
+++ public/cadastro.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Cadastro</title>
+</head>
+<body>
+    <form onsubmit="cadastroUsuario()">
+            <label>Nome</label>
+                <input type="text" name="nome" id="nome" required></br>
+            <label>CPF</label>
+                <input type="text" name="cpf" id="cpf" required></br>
+            <label>Data de Nascimento </label>
+                <input type="text" name="data_nascimento" id="data_nascimento" required></br>
+            <label>Telefone</label>
+                <input type="text" name="telefone" id="telefone" required></br>
+            <label>E-mail</label>
+                <input type="email" name="email" id="email" required></br>      
+            <label>Senha</label>
+                <input type="password" name="senha" id="senha" required></br>
+            <label>Endereço</label>
+                <input type="text" name="endereco" id="endereco" required></br>
+            <input type="submit" id="send" value="Cadastrar">
+    </form>
+
+    <p>Fazer <a href="http://localhost:8080/ProvaFullStack2/public/index.php"> login</a></p>
+
+
+    <!-- CHAMANDO SCRIPTS JS-->
+    <script src="../resources/js/post.js"></script>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -4,30 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parte 02</title>
+    <title>Página de Login</title>
 </head>
 <body>
-    <form onsubmit="cadastroUsuario()">
-            <label>Nome</label>
-                <input type="text" name="nome" id="nome" required></br>
-            <label>CPF</label>
-                <input type="text" name="cpf" id="cpf" required></br>
-            <label>Data de Nascimento </label>
-                <input type="text" name="data_nascimento" id="data_nascimento" required></br>
-            <label>Telefone</label>
-                <input type="text" name="telefone" id="telefone" required></br>
+
+    <form>
             <label>E-mail</label>
                 <input type="email" name="email" id="email" required></br>      
             <label>Senha</label>
                 <input type="password" name="senha" id="senha" required></br>
-            <label>Endereço</label>
-                <input type="text" name="endereco" id="endereco" required></br>
-            <input type="submit" id="send" value="Cadastrar">
+            <input type="submit" id="send" value="Entrar">
     </form>
+
+    <p>Ainda não possui <a href="http://localhost:8080/ProvaFullStack2/public/cadastro.php">cadastro ?</a></p>
+    
+
 
 
     <!-- CHAMANDO SCRIPTS JS-->
-    <script src="./resources/post.js"></script>
-    <script src="./resources/get.js"></script>
+    <script src="../resources/js/post.js"></script>
 </body>
 </html>
