e438a7430a4ac66235a8266cb2b4c943213ea99c
JulioCesarSousaMelo <jcesarsm17@gmail.com>
2021-08-27 06:15:44 -0300

    ==public + alteracao no action em upload_certificado.php + tratamento do formulario em main.php

--- public/main.php
+++ public/main.php
@@ -11,6 +11,21 @@
 
     }
 ?>
+<?php
+    include '../vendor/autoload.php'; // incluindo o auto-load
+
+    use phpseclib3\File\X509; // usando a biblioteca phpseclib
+
+    $x509 = new X509(); // instanciando um novo objeto
+    $aux = $x509->loadX509(file_get_contents('../teste.pem')); //lendo o certificado .pem  & armazendando resultado na variavel $aux
+
+    $dn = ($x509->getDN()); // armazenando o valor de DN
+
+    $issuerDN = ($x509->getIssuerDN()); // armazenando o valor de Issuer DN
+
+    $validityBefore = ($aux['tbsCertificate']['validity']['notBefore']['utcTime']); // armazenando a VALIDADE - NOT BEFORE
+    $validityAfter = ($aux['tbsCertificate']['validity']['notAfter']['utcTime']); // armazenando a VALIDADE - NOT AFTER 
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
--- public/upload_certificado.php
+++ public/upload_certificado.php
@@ -13,24 +13,6 @@
 
     $nome = $_SESSION['nome']; // Armazenando nome do usuário para exibir
 ?>
-<?php
-
-    include '../vendor/autoload.php'; // incluindo o auto-load
-
-    use phpseclib3\File\X509; // usando a biblioteca phpseclib
-
-    $x509 = new X509(); // instanciando um novo objeto
-    $aux = $x509->loadX509(file_get_contents('../teste.pem')); //lendo o certificado .pem  & armazendando resultado na variavel $aux
-    
-    $dn = ($x509->getDN()); // armazenando o valor de DN
-
-    $issuerDN = ($x509->getIssuerDN()); // armazenando o valor de Issuer DN
-
-    $validityBefore = ($aux['tbsCertificate']['validity']['notBefore']['utcTime']); // armazenando a VALIDADE - NOT BEFORE
-    $validityAfter = ($aux['tbsCertificate']['validity']['notAfter']['utcTime']); // armazenando a VALIDADE - NOT AFTER 
-
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -42,7 +24,7 @@
 <body>
     <h1>UPLOAD DE CERTIFICADO</h1>
 
-    <form action="../public/upload_certificado.php" method="POST">
+    <form action="../public/main.php" method="POST">
         <label for="nome_arquivo">Nome arquivo: </label>
             <input type="text" name="nome_arquivo" placeholder="ex: documento2021"><br>
         <label for="arquivo">Arquivo: </label>
