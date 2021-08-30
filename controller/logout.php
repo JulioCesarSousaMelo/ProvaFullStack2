20c5e2673fe6d9f22b216eabb780548a5a88d76d
JulioCesarSousaMelo <jcesarsm17@gmail.com>
2021-08-30 00:45:15 -0300

    ==controller + adicao do sistema de log em logout.php

--- controller/logout.php
+++ controller/logout.php
@@ -1,5 +1,26 @@
 <?php 
 
-session_start();
-session_destroy();
-header("Location: ../index.php");
+    session_start(); //  Iniciando sessão ou resumindo sessão existe   
+
+    include_once '../model/logs.php'; 
+
+    // Instanciando Objeto
+    $logs = new Logs(); 
+
+    // parâmetro para mensagemUpload()
+    $nome = $_SESSION['nome']; 
+
+    // .............................................................. //
+    //  Fazendo Logout no sistema                                     //
+    // .............................................................. //
+    session_destroy();
+
+    // .............................................................. //
+    //  Definições para a criação do Log de Logout                    //
+    // .............................................................. //
+    date_default_timezone_set('America/Sao_Paulo'); // Definindo o fuso horário de São Paulo
+
+    $mensagemLog = $logs->mensagemLogout($nome);  // Criando Mensagem para o Log
+    $logs->criarLog($mensagemLog);  // Criando Log
+    
+    header("Location: ../index.php");
