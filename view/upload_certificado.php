c5a5d37576b6b4539063b010a7b4b2f413de6d2e
JulioCesarSousaMelo <jcesarsm17@gmail.com>
2021-08-29 22:43:09 -0300

    ==view + alteracao no sistema de upload em upload_certificado.php

--- view/upload_certificado.php
+++ view/upload_certificado.php
@@ -10,53 +10,6 @@
                 window.location.href='../index.php';
               </script>";
     }
-
-    include_once '../vendor/autoload.php'; 
-    include_once '../config/database.php';
-    include_once '../model/certificado.php'; 
-    
-    use phpseclib3\File\X509; 
-
-    // Instanciando Novos Objetos 
-    $x509 = new X509(); 
-    $database = new Database();
-    $db = $database->getConnection();
-    $certificado = new Certificado($db);
-
-    if(isset($_POST['enviarArquivo'])){
-
-        $resultForm = $_POST['enviarArquivo'];
-        $nomeCertificado = $certificado->uploadArquivo($resultForm);
-
-        $resultado = $x509->loadX509(file_get_contents("../storage/certificado/$nomeCertificado")); 
-
-        $dn[] = ($x509->getDN()); // armazenando o valor de DN 
-        $issuerDN[] = ($x509->getIssuerDN()); // armazenando o valor de Issuer DN
-        $validityBefore = ($resultado['tbsCertificate']['validity']['notBefore']['utcTime']); // armazenando a VALIDADE - NOT BEFORE
-        $validityAfter = ($resultado['tbsCertificate']['validity']['notAfter']['utcTime']); // armazenando a VALIDADE - NOT AFTER 
-
-        $itemDN = array();
-        $itemIssuerDN = array();
-
-        // items DN
-        for($i = 0; $i < 6; $i++){
-            array_push($itemDN, $dn[0]["rdnSequence"][$i][0]["value"]["printableString"]);
-        }
-
-        // items issuerDN
-        for($i = 0; $i < 5; $i++){
-            array_push($itemIssuerDN, $dn[0]["rdnSequence"][$i][0]["value"]["printableString"]);
-        }
-
-        // trasformando os item array para string, pois os mesmos serão enviado ao banco de dados
-        $itemDN = implode(',', $itemDN);
-        $itemIssuerDN = implode(',', $itemIssuerDN);
-
-        $certificado->armazenarInformacoes($itemDN, $itemIssuerDN, $validityBefore, $validityAfter);
-
-        $id = $_SESSION['id'];
-        $certificado->inserirInformacoes($id);
-    }
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -71,7 +24,7 @@
 
     <p>Olá, <?= $_SESSION['nome']; ?> | <a href="../controller/logout.php">Logout</a></p> 
 
-    <form action= "<?php echo $_SERVER['PHP_SELF']; ?>" method= "POST" enctype= "multipart/form-data">
+    <form action= "<?php echo "../controller/enviar_certificado.php"; ?>" method= "POST" enctype= "multipart/form-data">
         <input type="file" name="file"/><br><br>
         <input type="submit" name="enviarArquivo" value="Enviar"/><br><br>
     </form>
